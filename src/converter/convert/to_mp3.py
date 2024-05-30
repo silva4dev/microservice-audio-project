353c4a12b1e9616cb579fe24301d15ec34f3a042
silva4dev <web4dev.lucas@outlook.com>
2024-05-30 18:31:36 -0300

    feat: add to mp3

--- src/converter/convert/to_mp3.py
+++ src/converter/convert/to_mp3.py
@@ -6,21 +6,16 @@ import moviepy.editor
 def start(message, fs_videos, fs_mp3s, channel):
     message = json.loads(message)
 
-    # empty temp file
     tf = tempfile.NamedTemporaryFile()
-    # video contents
     out = fs_videos.get(ObjectId(message["video_fid"]))
-    # add video contents to empty file
+
     tf.write(out.read())
-    # create audio from temp video file
     audio = moviepy.editor.VideoFileClip(tf.name).audio
     tf.close()
 
-    # write audio to the file
     tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
     audio.write_audiofile(tf_path)
 
-    # save file to mongo
     f = open(tf_path, "rb")
     data = f.read()
     fid = fs_mp3s.put(data)
