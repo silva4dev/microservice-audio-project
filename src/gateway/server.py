47507f1316c2d1b54c8b8f2658cfff2c65c5c54d
silva4dev <web4dev.lucas@outlook.com>
2024-05-30 18:33:07 -0300

    feat: add server

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -18,49 +18,65 @@ fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
 
+
 @server.route("/login", methods=["POST"])
 def login():
     token, err = access.login(request)
+
     if not err:
         return token
     else:
         return err
 
+
 @server.route("/upload", methods=["POST"])
 def upload():
     access, err = validate.token(request)
+
     if err:
         return err
+
     access = json.loads(access)
+
     if access["admin"]:
         if len(request.files) > 1 or len(request.files) < 1:
             return "exactly 1 file required", 400
+
         for _, f in request.files.items():
             err = util.upload(f, fs_videos, channel, access)
+
             if err:
                 return err
+
         return "success!", 200
     else:
         return "not authorized", 401
 
+
 @server.route("/download", methods=["GET"])
 def download():
     access, err = validate.token(request)
+
     if err:
         return err
 
     access = json.loads(access)
+
     if access["admin"]:
         fid_string = request.args.get("fid")
+
         if not fid_string:
             return "fid is required", 400
+
         try:
             out = fs_mp3s.get(ObjectId(fid_string))
             return send_file(out, download_name=f"{fid_string}.mp3")
         except Exception as err:
             print(err)
             return "internal server error", 500
+
     return "not authorized", 401
+
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=8080)
